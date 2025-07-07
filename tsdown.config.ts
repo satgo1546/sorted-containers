65fb676c1618f7e22f9e5b97c83c26b88a9a968b
Frog Chen <satgo1546@yeah.net>
2025-07-07 12:21:32 +0800

    Polyfill iterator types for TypeScript < 5.6 (#2)

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from 'tsdown'
+import { readFileSync, writeFileSync } from 'node:fs'
 
 export default defineConfig({
 	entry: ['src/index.ts'],
@@ -8,5 +9,25 @@ export default defineConfig({
 			options.name = 'SortedContainers'
 			options.minify = true
 		}
+	},
+	onSuccess() {
+		writeFileSync('dist/index.ts4.5.d.ts', `// polyfill types for TypeScript < 5.6
+interface IteratorObject<T, TReturn = unknown, TNext = unknown> extends Iterator<T, TReturn, TNext> {
+	[Symbol.iterator](): IteratorObject<T, TReturn, TNext>
+}
+
+interface ArrayIterator<T> extends IteratorObject<T, any, unknown> {
+	[Symbol.iterator](): ArrayIterator<T>
+}
+
+interface SetIterator<T> extends IteratorObject<T, any, unknown> {
+	[Symbol.iterator](): SetIterator<T>
+}
+
+interface MapIterator<T> extends IteratorObject<T, any, unknown> {
+	[Symbol.iterator](): MapIterator<T>
+}
+// polyfill end
+` + readFileSync('dist/index.d.ts'))
 	},
 })
